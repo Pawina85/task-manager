81dec7dda5135b270906b5464cbc8d8c58d6377f
Pawina85 <pawinachanthachon@gmail.com>
2025-12-12 15:54:22 +0700

    Homwpage details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import TaskForm from "./components/TaskForm";
+
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(text) {
+    const newTask = {
+      id: Date.now(),
+      text: text,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div style={{ padding: "20px" }}>
+      <h1>Task Manager App</h1>
+
+      <TaskForm onAddTask={addTask} />
+
+      <pre>{JSON.stringify(tasks, null, 2)}</pre> {/* temporary display */}
     </div>
   );
 }
