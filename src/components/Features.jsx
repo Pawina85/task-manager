8b8613f5fbdc0cb871e9e58d360f2445100a44e0
Pawina85 <pawinachanthachon@gmail.com>
2025-12-18 20:51:47 +0700

    Full step Dashboard ubdate

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -105,7 +105,7 @@ const imageContainerStyle = {
 const imageStyle = {
     width: '100%',
     height: '100%',
-    objectFit: 'contain',
+    objectFit: 'cover-fit',
 }
 
 const ctaContainerStyle = {
